2dac2ff61e6d2ab711d91d12c62201daa38bf41d
DanielReyes03 <daniel_reyes2003@outlook.com>
2023-05-12 10:07:22 -0600

    moviendo los archivos a un nuevo paquete y agregando controlar de menu

---
+++ src/menu/controlador.java
@@ -0,0 +1,23 @@
+package menu;
+public class controlador {
+
+    public void controladorIngresos(){
+        System.out.println("Estoy en ingresos");
+    }
+
+    public void controladorRecursos(){
+        System.out.println("Estoy en recursos");
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
+
+
+
